82704a9ca34523715f8d9bf5e9c2f80ff8bc02f7
Jordan Femi_Bella <femibellajordan@gmail.com>
2023-07-14 21:53:20 +0100

    Completed day 1 streak

---
+++ area-of-circle.py
@@ -0,0 +1,10 @@
+import math as M
+
+# Get radius from user input
+Radius = float(input("Please enter the radius of your circle: "))
+
+# Calculate the area of the circle
+area_of_the_circle = M.pi * Radius * Radius
+
+# Display the area of the circle
+print("The area of your circle is:", area_of_the_circle)
