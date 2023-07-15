7f3cbbfe54449e5755bc4e8ec4434c5fe0413912
Jordan Femi-Bella <femibellajordan@gmail.com>
2023-07-15 11:26:13 +0100

    Completed day 2

---
+++ python/color_list.py
@@ -0,0 +1,9 @@
+color_list = ["Red","Green","White" ,"Black"]
+
+#Display the first color in the list
+print("First color is:", color_list[0])
+
+#Display the last color in the list
+print("Last color is:", color_list[-1])
+
+
