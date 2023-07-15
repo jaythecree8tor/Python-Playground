f342c8e48e4d7b9d36106365babbd8d73154d8b9
Jordan Femi-Bella <femibellajordan@gmail.com>
2023-07-15 11:39:15 +0100

    Completed day 2

--- python/convert_to_list_and_tuple.py
+++ python/convert_to_list_and_tuple.py
@@ -1,8 +1,8 @@
 # Prompt the user to enter a list of numbers separated by commas
-values = input("Enter some numbers separated by commas: ")
+user_input = input("Enter some numbers separated by commas: ")
 
 # Split the input string into a list of numbers
-number_list = values.split(",")
+number_list = user_input.split(",")
 
 # Convert the list into a tuple
 number_tuple = tuple(number_list)
