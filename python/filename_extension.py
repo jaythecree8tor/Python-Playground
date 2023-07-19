d021b2e108e11e71f8b5f0e1819916c68728f99a
Jordan Femi-Bella <femibellajordan@gmail.com>
2023-07-19 06:10:29 +0100

    Completed day 6

--- python/filename_extension.py
+++ python/filename_extension.py
@@ -19,3 +19,31 @@ name, extension = filename.split(".")
 
 # Print the file extension
 print("File extension:", extension)
+
+#OR
+
+Extensive_Name = input("Enter Your File Name: ")
+dot_index = Extensive_Name.find(".")
+if dot_index != -1:
+    print("You've Inputted  a ."+Extensive_Name[dot_index + 1:] + " File")
+else:
+    print("No dot (.) found in the file name.")
+
+#Third Method
+def print_value_after_dot(string):
+  """Prints any value after the "." sign.
+
+  Args:
+    string: The string to print the value from.
+  """
+
+  index = string.find(".")
+  if index == -1:
+    print("The string does not contain a '.'")
+  else:
+    print(string[index + 1:])
+
+
+if __name__ == "__main__":
+  string = input("Enter Your File Name: ")
+  print_value_after_dot(string)
